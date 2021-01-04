6fc466880ef8beb784c6a2bba0297b814cf3753d
qwfy5287 <qwfy5287@qq.com>
2021-01-04 11:43:01 +0800

    feat: ElementPlus error

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+import ElementPlus from "element-plus";
+// // import "element-plus/lib/theme-chalk/index.css";
+// import App from "./App.vue";
+
+// // import { index } from "element-plus/lib/theme-chalk";
+
+const app = createApp(App);
+app.use(ElementPlus);
+app.mount("#app");

8b89c9d5ea32692b6689c1a918780f86bc427253
qwfy5287 <qwfy5287@qq.com>
2021-01-04 11:05:27 +0800

    feat: antd 2 rc.7 error

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+import Antd from "ant-design-vue";
+import App from "./App";
+import "ant-design-vue/dist/antd.css";
+
+const app = createApp();
+app.config.productionTip = false;
+
+app.use(Antd);
